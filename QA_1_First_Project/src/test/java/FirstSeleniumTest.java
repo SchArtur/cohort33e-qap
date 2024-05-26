925ad76ad3a8667b339e47c3f701929276742dc1
Natalja-Markova <nataljamarkova71@gmail.com>
2024-05-26 21:57:49 +0200

    Homework 2 (изменено)

--- QA_1_First_Project/src/test/java/FirstSeleniumTest.java
+++ QA_1_First_Project/src/test/java/FirstSeleniumTest.java
@@ -1,2 +1,21 @@
-public class FirstSeleniumTest {
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.*;
+
+public class FirstSeleniumTest extends BaseTest {
+
+    @Test
+    @DisplayName("Проверка заголовка страницы")
+    void testOpenBrowser() {
+        driver.get("https://www.google.com/");//Открытие страницы
+
+        driver.navigate().back();//Возврат на предыдущую страницу
+        driver.navigate().forward();//Переход на следующую страницу
+        driver.navigate().refresh();//Обновление страницы
+        Assertions.assertEquals("Google", driver.getTitle(), "Открыта страница с другим Title");//Проверка заголовка страницы
+    }
 }
