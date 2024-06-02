d5125cfd21a67451ca1c5518496f8839e1f64f7c
Andrii <143963266+andriib77@users.noreply.github.com>
2024-06-02 15:33:31 +0200

    Homework6 AddItemToCartoCart

---
+++ QA33_DemoShop/src/test/java/com/webshop/tests/AddItemToCart.java
@@ -0,0 +1,46 @@
+package com.webshop.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AddItemToCart extends BaseTest{
+
+    @BeforeEach
+    void preconditions(){
+        loginTestUser();
+    }
+
+    @AfterEach
+    void quitDriver(){ driver.quit();}
+
+    @Test
+    @DisplayName("Добавление товара в корзину и очистка корзины")
+    void addItem (){
+
+    /*driver.findElements(By.cssSelector("[class='product-item']")).get(1).findElement(By.cssSelector("[value='Add to cart']")).click();*/
+
+        driver.findElements(By.cssSelector("[class='product-item']")).get(1).click();
+        clickOnElement(By.cssSelector("[value='Add to cart']"));
+        waitInSeconds(2);
+        clickOnElement(By.cssSelector("[class='cart-label']"));
+        assertTrue(getElementBy(By.xpath("//input[@class='qty-input']")).isEnabled(),String.format("Товар не добавлен в корзину"));
+        clickOnElement(By.cssSelector("[name='removefromcart']"));
+        clickOnElement(By.cssSelector("[name='updatecart']"));
+
+        assertTrue(getElementBy(By.cssSelector("[class='order-summary-content']")).isDisplayed());
+        clickOnElement(By.cssSelector("[href='/']"));
+        clickOnElement(By.xpath("//a[@class='ico-logout']"));
+    }
+
+    @Test
+    @DisplayName("Проверка пустой корзины")
+    void checkCart(){
+        clickOnElement(By.cssSelector("[class='cart-label']"));
+        assertTrue(getElementBy(By.cssSelector("[class='order-summary-content']")).isDisplayed());
+    }
+}
