67b963d938fb83bb5c60761867e7dab7733240eb
OlgaMeier <147302558+OlgaMeier@users.noreply.github.com>
2024-06-03 18:05:37 +0200

    AddItemToCartTest version 2 03.06.2024

--- QA_33_DemoShop/src/test/java/AddToCartTest.java
+++ QA_33_DemoShop/src/test/java/AddToCartTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import static org.openqa.selenium.By.cssSelector;
+
 
 public class AddToCartTest extends TestBase {
 
@@ -14,43 +16,63 @@ public class AddToCartTest extends TestBase {
 
         //открываем страницу, переходим на элемет Log in
         driver.get(DEMO_SHOP_URL);
-        clickOnElement(By.cssSelector("[class=\"ico-login\"]"));
+        clickOnElement(cssSelector("[class=\"ico-login\"]"));
         //заполняем поля формы Log in для входа в аккаунт
 
         fillInputField(By.id("Email"), userForm.getEmail());
         fillInputField(By.id("Password"), userForm.getPassword());
         // нажимаем кнопку Log in для подтверждения входа
-        clickOnElement(By.cssSelector("[class='button-1 login-button']"));
+        clickOnElement(cssSelector("[class='button-1 login-button']"));
 
         // Убеждаемся в успешном логировании, тем что иконка Log in сменилась на Log out
-        WebElement confirmationLogOut = getElementBy(By.cssSelector("[class='ico-logout']"));
+        WebElement confirmationLogOut = getElementBy(cssSelector("[class='ico-logout']"));
         Assertions.assertEquals("Log out", confirmationLogOut.getText(), "Icon is not Log out");
     }
 
+    @Test
+    @DisplayName("переходим на главную страницу , находим козину и проверяем, что корзина пуста, если нет , то очищаем корзину")
     void checkAndClearShoppingCart() {
         driver.get(DEMO_SHOP_URL);
 
-        //переходим на главную страницу , находим козину и проверяем, что корзина пуста, если нет , то очищаем корзину
-        WebElement shoppingCart = getElementBy(By.cssSelector("[class='cart-label']"));
+        WebElement shoppingCart = getElementBy(cssSelector("[class='cart-label']"));
         Assertions.assertEquals("Shopping cart", shoppingCart.getText(), "Shopping cart is not found");
 
-        WebElement shoppingCartItems = getElementBy(By.cssSelector("[class='cart-qty']"));
-        String cartQtyText = shoppingCartItems.getText();
-        int cartQty = Integer.parseInt(cartQtyText.substring(1, cartQtyText.length() - 1));
-        if (cartQty > 0) {
-            shoppingCartItems.clear();
-        }
+     /*  private int getCartQuantity () {
+           WebElement cartQtyElement = getElementBy(By.className("cart-qty"));// '(0)'
+           String cartQtyString = cartQtyElement.getText().replace("(", "").replace(")", ""); // 0
+    //    String cartQtyString = cartQtyElement.getText().replaceAll("\\D", "");// '0' - заменили все символы '(' и ')'
+           return Integer.parseInt(cartQtyString);
+       }*/
+
+    /* int CartQty= getCartQuantity(){
+           WebElement shoppingCartItems = getElementBy(By.cssSelector("[class='cart-qty']"));
+           String cartQtyText = shoppingCartItems.getText();
+           int cartQty = Integer.parseInt(cartQtyText.substring(1, cartQtyText.length() - 1));
+           if (cartQty > 0) {
+               shoppingCartItems.clear();
+           }
+           int i = Integer.parseInt(cartQty);
+           return i;
+       }*/
+
+
         //переходим на главную страницу и добавляем выбранный товар в корзину
-        getElementBy(By.cssSelector("[alt='Tricentis Demo Web Shop']"));
-        WebElement cartItem = getElementBy(By.cssSelector("[itemprop='name']"));
-        Assertions.assertEquals("14.1-inch Laptop", cartItem.getText(), "Cart item is not '14.1-inch Laptop'");
-        clickOnElement(By.cssSelector("[id='add-to-cart-button-31']"));
+
+        WebElement cartItem = getElementBy(By.xpath("//div[@class= 'button']/descendant::class='button-2 product-box-add-to-cart-button']")).click();
+        Assertions.assertEquals("Add to cart", cartItem.getText(), "Cart item is not '14.1-inch Laptop'");
 
         //переходим в корзину для проверки добавлен ли выбранный товар
-        clickOnElement(By.cssSelector("[class='cart-qty']"));
-        getElementBy(By.cssSelector("[class='product-name']"));
+        clickOnElement(cssSelector("[class='cart-qty']"));
+        getElementBy(cssSelector("[class='product-name']"));
         Assertions.assertEquals("14.1-inch Laptop", cartItem.getText(), "Cart item is not '14.1-inch Laptop'");
+
+   /* private int getCartQuantity() {
+        WebElement cartQtyElement = getElementBy(By.className("cart-qty"));// '(0)'
+        String cartQtyString = cartQtyElement.getText().replace("(","").replace(")",""); // 0
+//        String cartQtyString = cartQtyElement.getText().replaceAll("\\D", "");// '0' - заменили все символы '(' и ')'
+        return Integer.parseInt(cartQtyString);*/
     }
+
 }
 
 
