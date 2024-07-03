ed7f5cf51d2a2aeb326dcb02a4e0c0f5532a8054
OlgaMeier <147302558+OlgaMeier@users.noreply.github.com>
2024-07-03 15:01:26 +0200

    QA_33_FirstAppiumTest _ ColomborTest_HW  failed 03.07.2024

---
+++ QA33_FirstAPIUMTEST/src/test/java/AppiumStartServer.java
@@ -0,0 +1,39 @@
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+import java.io.File;
+
+public class AppiumStartServer {
+
+
+    //Сам аппиум сервер
+    private static AppiumDriverLocalService service;
+
+    public static void startServer() {
+        //Создаем билдер с настройками нашего appium сервера
+        AppiumServiceBuilder builder = new AppiumServiceBuilder();
+        // Устанавливаем путь до appium (cmd: where appium)
+        builder.withAppiumJS(new File("/opt/homebrew/bin/appium"));
+        // Указываем адрес и порт
+        builder.withIPAddress("127.0.0.1").usingPort(4723);
+
+        service = AppiumDriverLocalService.buildService(builder);
+        // Запускаем сервер
+        service.start();
+        //выводим в консоль инфо, что сервер запущен
+        if (service.isRunning()) {
+            System.out.println("Server started - ok");
+        } else {
+            System.out.println("Server started - failed");
+        }
+    }
+
+    public static void stopServer() {
+        if (service != null && service.isRunning()) {
+            service.stop();
+            System.out.println("Server stopped");
+        }
+    }
+}
+
+
